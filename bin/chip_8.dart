1e41c09f551ea67b53d4055b4432de1e22a429ce
Vincent Engel <ve@vesion.io>
2021-08-03 00:49:51 +0200

    Added basic CLI

--- bin/chip_8.dart
+++ bin/chip_8.dart
@@ -1,3 +1,21 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+import 'dart:io';
+
+import 'package:chip_8/chip_8.dart';
+
+import 'src/debug_mod.dart';
+
+Future<void> main(List<String> arguments) async {
+  final rom = await loadRom(arguments[0]);
+  final chip8 = Chip8(rom, mods: [DebugMod()]);
+
+  chip8.start();
+}
+
+Future<List<int>> loadRom(String romLocation) async {
+  final romFile = File(romLocation);
+  if (!await romFile.exists()) {
+    throw Exception('ROM not found: ' + romLocation);
+  }
+
+  return await romFile.readAsBytes();
 }
